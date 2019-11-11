ae122ca9a5c86f3f5fb37b6e53b574dd3472f8ee
root <root@ip-172-31-12-115.sa-east-1.compute.internal>
2019-11-11 17:38:11 -0200

    Atualizando as wikis

--- dokuwiki/conf/users.auth.php
+++ dokuwiki/conf/users.auth.php
@@ -9,5 +9,4 @@
 # login:passwordhash:Real Name:email:groups,comma,separated
 
 
-teste:$1$3zY3LFLQ$zQBklVzrNRuJA1zR2Hwfw1:Teste:teste@teste.com:usuarios
 cloudfy:$1$YK4yxaGQ$Tw9riB.hC0A6R2/Tt78zd.:Suporte Cloudfy:nelson.pires@cloudfy.net.br:admin,user
